4cbe5c932b7b1d7f49b4373d8b3b9c40fecbb70c
Ivan Itzcovich <i.itzcovich@gmail.com>
2016-11-10 17:39:44 -0300

    Add Data model

--- client/src/main/java/ar/edu/itba/pod/hz/client/Client.java
+++ client/src/main/java/ar/edu/itba/pod/hz/client/Client.java
@@ -1,12 +1,81 @@
 package ar.edu.itba.pod.hz.client;
 
+import java.util.concurrent.ExecutionException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Client {
-    private static Logger logger = LoggerFactory.getLogger(Client.class);
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.config.ClientNetworkConfig;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
 
-    public static void main(String[] args) {
-        logger.info("clase09-ejer01 Client Starting ...");
-    }
+import ar.edu.itba.pod.hz.client.reader.DataSetReader;
+import ar.edu.itba.pod.hz.model.Data;
+
+public class Client {
+	private static final String MAP_NAME = "mapa";
+	private static Logger logger = LoggerFactory.getLogger(Client.class);
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+		String name = System.getProperty("name");
+		String pass = System.getProperty("pass");
+		if (pass == null) {
+			pass = "dev-pass";
+		}
+
+		System.out.println(String.format("Connecting with cluster dev-name [%s]", name));
+
+		ClientConfig ccfg = new ClientConfig();
+		ccfg.getGroupConfig().setName(name).setPassword(pass);
+
+		// no hay descubrimiento automatico,
+		// pero si no decimos nada intentará usar LOCALHOST
+		String addresses = System.getProperty("addresses");
+		if (addresses != null) {
+			String[] arrayAddresses = addresses.split("[,;]");
+			ClientNetworkConfig net = new ClientNetworkConfig();
+			net.addAddress(arrayAddresses);
+			ccfg.setNetworkConfig(net);
+		}
+		HazelcastInstance client = HazelcastClient.newHazelcastClient(ccfg);
+
+		System.out.println(client.getCluster());
+
+		// Preparar la particion de datos y distribuirla en el cluster a trav�s
+		// del IMap
+		IMap<String, Data> myMap = client.getMap(MAP_NAME);
+		try {
+			DataSetReader.readDataSet(myMap);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+		// // Ahora el JobTracker y los Workers!
+		// JobTracker tracker = client.getJobTracker("default");
+		//
+		// // Ahora el Job desde los pares(key, Value) que precisa MapReduce
+		// KeyValueSource<String, Votacion> source =
+		// KeyValueSource.fromMap(myMap);
+		// Job<String, Votacion> job = tracker.newJob(source);
+		//
+		// // // Orquestacion de Jobs y lanzamiento
+		// ICompletableFuture<Map<String, FormulaTupla>> future = job
+		// .mapper(new ComunaFormulaVotesMapperFactory())
+		// .reducer(new WinningFormulaReducerFactory())
+		// .submit();
+		//
+		// // Tomar resultado e Imprimirlo
+		// Map<String, FormulaTupla> rta = future.get();
+		//
+		// for (Entry<String, FormulaTupla> e : rta.entrySet()) {
+		// System.out.println(String.format("Distrito %s => Ganador %s",
+		// e.getKey(), e.getValue()));
+		// }
+
+		System.exit(0);
+
+	}
 }
---
+++ client/src/main/java/ar/edu/itba/pod/hz/client/reader/DataSetReader.java
@@ -0,0 +1,63 @@
+package ar.edu.itba.pod.hz.client.reader;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import org.supercsv.cellprocessor.ParseInt;
+import org.supercsv.cellprocessor.Trim;
+import org.supercsv.cellprocessor.constraint.NotNull;
+import org.supercsv.cellprocessor.ift.CellProcessor;
+import org.supercsv.io.CsvBeanReader;
+import org.supercsv.io.ICsvBeanReader;
+import org.supercsv.prefs.CsvPreference;
+
+import com.hazelcast.core.IMap;
+
+import ar.edu.itba.pod.hz.model.Data;
+import ar.edu.itba.pod.hz.model.Votacion;
+
+public class DataSetReader {
+	// el directorio wc dentro en un directorio llamado "resources"
+	// al mismo nivel que la carpeta src, etc.
+	private static final String FILENAME = "files/dataset-1000.csv";
+
+	private static CellProcessor[] getProcessors() {
+		return new CellProcessor[] { new ParseInt(new NotNull()), // tipovivienda
+				new ParseInt(new NotNull()), // calidadservicios
+				new ParseInt(new NotNull()), // sexo
+				new ParseInt(new NotNull()), // edad
+				new ParseInt(new NotNull()), // alfabetismo
+				new ParseInt(new NotNull()), // actividad
+				new NotNull(new Trim()), // nombredepto
+				new NotNull(new Trim()), // nombreprov
+				new ParseInt(new NotNull()) // hogarid
+		};
+	}
+
+	public static void readDataSet(final IMap<String, Data> theIMap) throws Exception {
+		ICsvBeanReader beanReader = null;
+		try {
+			final InputStream is = DataSetReader.class.getClassLoader().getResourceAsStream(FILENAME);
+			final Reader aReader = new InputStreamReader(is);
+			beanReader = new CsvBeanReader(aReader, CsvPreference.EXCEL_PREFERENCE); // separador
+																						// ,
+
+			// the header elements are used to map the values to the bean (names
+			// must match)
+			final String[] header = beanReader.getHeader(true);
+			final CellProcessor[] processors = getProcessors();
+
+			Data aD;
+			while ((aD = beanReader.read(Data.class, header, processors)) != null) {
+				System.out.println(String.format("lineNo=%s, rowNo=%s, customer=%s", beanReader.getLineNumber(),
+						beanReader.getRowNumber(), aD));
+//				 theIMap.set(aV.getDistrito() + "-" + aV.getFormula(), aV);
+			}
+		} finally {
+			if (beanReader != null) {
+				beanReader.close();
+			}
+		}
+	}
+}
